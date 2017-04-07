127e03cd868a0f533de294469ab071483ec4725d
sayeed99 <sayeed.afridi2009@gmail.com>
2017-04-07 22:29:48 +0530

    libshim : shim sensors
    Change-Id: I2e4c8189b0b40253772979078ec1326416a3f7dc

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -39,3 +39,15 @@ LOCAL_MODULE := libshim_gpsd
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
+
+#Sensor
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := sensors_shim.cpp
+LOCAL_SHARED_LIBRARIES := libgui
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)
+LOCAL_MODULE := libshims_sensors
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
