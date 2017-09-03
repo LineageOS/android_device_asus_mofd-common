e91cf7c7f6a33c36328bd9684e6d60d9f2b95519
jrior001 <jriordan001@gmail.com>
2017-09-03 10:42:53 -0400

    mofd: move to blob for tinyalsa
    Change-Id: Ia67e4416013ffd5a204b8ddd3baa151ed9901550

--- mofd.mk
+++ mofd.mk
@@ -36,7 +36,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio@2.0-impl \
     android.hardware.audio.effect@2.0-impl \
     libtinycompress \
-    libtinyalsa \
     audio.a2dp.default \
     audio.r_submix.default \
     audio.usb.default
